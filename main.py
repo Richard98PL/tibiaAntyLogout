760fb09863ec173efff61a6314c0f9404b2b6d8c
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2021-10-31 09:33:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,63 @@
+from pynput.keyboard import Key, Controller
+import win32gui
+import re
+import time
+import schedule
+import datetime
+from random import randint
+keyboard = Controller()
+
+class WindowMgr:
+    """Encapsulates some calls to the winapi for window management"""
+
+    def __init__ (self):
+        """Constructor"""
+        self._handle = None
+
+    def find_window(self, class_name, window_name=None):
+        """find a window by its class_name"""
+        self._handle = win32gui.FindWindow(class_name, window_name)
+
+    def _window_enum_callback(self, hwnd, wildcard):
+        """Pass to win32gui.EnumWindows() to check all the opened windows"""
+        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
+            self._handle = hwnd
+
+    def find_window_wildcard(self, wildcard):
+        """find a window whose title matches the wildcard regex"""
+        self._handle = None
+        win32gui.EnumWindows(self._window_enum_callback, wildcard)
+
+    def set_foreground(self):
+        """put the window in the foreground"""
+        win32gui.SetForegroundWindow(self._handle)
+
+    def get_rectangle(self):
+        win32gui.GetWindowRect(self._handle)
+
+#hwndMain = w._handle
+
+def antyLogout():
+    global w
+    w = WindowMgr()
+    w.find_window_wildcard(".*Tibia.*")
+    w.set_foreground()
+
+    keyboard.press(Key.ctrl)
+    movementKeys = [Key.up, Key.down]
+    for key in movementKeys:
+        keyboard.tap(key)
+        time.sleep( randint(15,31) / 1000)
+    keyboard.release(Key.ctrl)
+    now = datetime.datetime.now()
+    print(now.hour, now.minute, now.second)
+    
+    
+
+antyLogout()
+schedule.every(5).minutes.do(antyLogout)
+while 1:
+    schedule.run_pending()
+    time.sleep(60)
+
+
